e0e3a73ee85ca9ef3fc48348c0c38fa126966274
qkdwngus <39788908+arkhyeon@users.noreply.github.com>
2022-06-29 17:38:33 +0900

    [feat] Header Setting

---
+++ src/template/Header.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import { MdSearch } from "react-icons/all";
+import styled from "@emotion/styled";
+import { NavLink } from "react-router-dom";
+
+function Header() {
+  return (
+    <HeaderWrap>
+      <MenuWrap>
+        <img src="src/resource/img/logo.png" alt="logo" />
+        <NavLink to="/trend">
+          <li>Trend</li>
+        </NavLink>
+        <NavLink to="/movie">
+          <li>Movie</li>
+        </NavLink>
+        <NavLink to="/series">
+          <li>Series</li>
+        </NavLink>
+        <NavLink to="/library">
+          <li>Library</li>
+        </NavLink>
+      </MenuWrap>
+      <SubMenuWrap>
+        <MdSearch />
+        <img src="src/resource/img/user1.jpg" alt="?" />
+        <p>User Name</p>
+      </SubMenuWrap>
+    </HeaderWrap>
+  );
+}
+
+const HeaderWrap = styled.div`
+  width: 100%;
+  height: 56px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const MenuWrap = styled.ul`
+  display: flex;
+  text-align: center;
+  line-height: 56px;
+  & a {
+    color: #ebebeb;
+    font-size: 18px;
+    font-weight: 600;
+    padding: 0 3rem;
+
+    &:hover {
+      color: #676aa8;
+    }
+  }
+`;
+
+const SubMenuWrap = styled.div`
+  display: flex;
+  line-height: 56px;
+  color: #ebebeb;
+  margin-right: 1.5rem;
+  & img {
+    width: 40px;
+    height: 40px;
+    margin: 8px 8px 0 0;
+    border-radius: 100%;
+  }
+
+  & p {
+    width: 100px;
+  }
+
+  & svg {
+    width: 46px;
+    height: 28px;
+    margin: 14px auto;
+    cursor: pointer;
+  }
+`;
+
+export default Header;
---
+++ src/template/Index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./Header";
+
+const Index = ({ Outlet }) => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default Index;
