8c3fb4ab73bb39c8d3bdd629337cd6e5be22d84e
qkdwngus <39788908+arkhyeon@users.noreply.github.com>
2022-06-30 17:57:40 +0900

    [add] Unversioned Files PUSH
    -

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
+import Index from "./template/Index";
+import UserSelector from "./user/UserSelector";
+
+function App() {
+  const [movie, setMovie] = useState();
+
+  const getMovie = () => {
+    axios
+      .get(
+        "https://api.themoviedb.org/3/trending/all/day?api_key=e46e41278a82040a873227b4820d1e48"
+      )
+      .then((res) => {
+        console.log(res);
+        setMovie(res);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Index />}>
+          <Route path="user" element={<UserSelector />}></Route>
+        </Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
